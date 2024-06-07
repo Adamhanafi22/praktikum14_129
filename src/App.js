cbd87e05414a3f06e26366445844b3a334986540
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-06-07 21:59:08 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import logo from './4.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pemuda Pancasila</h1>
+        <div className="App-content">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Saya Adam Hanafi Putra Mahasiswa Teknologi Informasi
+            Anggota Pemuda Pancasila.
+          </p>
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
       </header>
+      <div className="App-footer">
+        <p>Footer content goes here.</p>
+      </div>
     </div>
   );
 }
